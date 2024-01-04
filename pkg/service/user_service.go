d9d5d71ab42b680ad7e5b5502ab84563b22d7270
freeez <coldinterest@foxmail.com>
2024-01-04 19:05:47 +0800

    update user repo

--- pkg/service/user_service.go
+++ pkg/service/user_service.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	UserModel "pharmacy-pos/pkg/db/models"
-	"pharmacy-pos/pkg/db/repository"
+	UserRepo "pharmacy-pos/pkg/db/repository"
 
 	"gorm.io/gorm"
 )
@@ -19,27 +19,27 @@ func NewUserService(db *gorm.DB) *UserService {
 
 // GetUserByID 根据用户ID获取用户
 func (us *UserService) GetUserByID(id uint) (*UserModel.User, error) {
-	return repository.GetUserByID(us.DB, id)
+	return UserRepo.GetUserByID(us.DB, id)
 }
 
 // CreateUser 创建新用户
 func (us *UserService) CreateUser(user *UserModel.User) error {
-	return repository.CreateUser(us.DB, user)
+	return UserRepo.CreateUser(us.DB, user)
 }
 
 // UpdateUser 更新用户信息
 func (us *UserService) UpdateUser(user *UserModel.User) error {
-	return repository.UpdateUser(us.DB, user)
+	return UserRepo.UpdateUser(us.DB, user)
 }
 
 // DeleteUserByID 根据ID删除用户
 func (us *UserService) DeleteUserByID(id uint) error {
-	return repository.DeleteUserByID(us.DB, id)
+	return UserRepo.DeleteUserByID(us.DB, id)
 }
 
 // AuthenticateUser 验证用户的用户名和密码
 func (us *UserService) AuthenticateUser(username, password string) (*UserModel.User, error) {
-	user, err := repository.GetUserByUserName(us.DB, username)
+	user, err := UserRepo.GetUserByUserName(us.DB, username)
 	if err != nil {
 		return nil, err
 	}
